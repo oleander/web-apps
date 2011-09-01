41e4edfa1e32b9a3c1d2504e293f6b3c8ff5e22c
Linus Oleander <linus@oleander.nu>
2011-09-01 14:17:38 +0200

    Adding HTML template

---
+++ workshops/html.nbp/src/main/java/edu/chl/oleander/html/nbp/App.java
@@ -0,0 +1,13 @@
+package edu.chl.oleander.html.nbp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
